e5250a240c3a96e2f44aab3745fed8815c228f02
BadKoldrysh <ohmygodkir17@gmail.com>
2019-07-04 21:01:12 +0200

    Add ground (but it doesn't work)

--- js/part1.js
+++ js/part1.js
@@ -15,7 +15,7 @@ function preload()
 {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('star', 'assets/star.png');
-    this.load.image('platform', 'assets/platform.png');
+    this.load.image('ground', 'assets/platform.png');
     this.load.image('dude',
                     'assets/dude.png', 
                     { frameWidth: 32, frameHeight: 48 }
@@ -26,7 +26,14 @@ function preload()
 function create()
 {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update()
