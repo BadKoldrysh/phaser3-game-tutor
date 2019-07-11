96f75011bf4533ce22fff103ac6f9cec81ff941e
BadKoldrysh <ohmygodkir17@gmail.com>
2019-07-12 00:01:05 +0200

    Move animations code from Scene2.js to Scene1.js

--- game-2/Scene1.js
+++ game-2/Scene1.js
@@ -9,37 +9,39 @@ class Scene1 extends Phaser.Scene {
     // args: (image ID, URL)
     this.load.image("background", "game-2/assets/background.png");
 
-    this.load.spritesheet("ship", "game-2/assets/spritesheets/ship.png",
-    {
+    this.load.spritesheet("ship", "game-2/assets/spritesheets/ship.png", {
       frameWidth: 16,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("ship2", "game-2/assets/spritesheets/ship2.png",
-    {
+    this.load.spritesheet("ship2", "game-2/assets/spritesheets/ship2.png", {
       frameWidth: 32,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("ship3", "game-2/assets/spritesheets/ship3.png",
-    {
+    this.load.spritesheet("ship3", "game-2/assets/spritesheets/ship3.png", {
       frameWidth: 32,
-      frameHeight: 32,
+      frameHeight: 32
     });
-    this.load.spritesheet("explosion", "game-2/assets/spritesheets/explosion.png",
-    {
-      frameWidth: 16,
-      frameHeight: 16,
-    });
-    this.load.spritesheet("power-up", "game-2/assets/spritesheets/power-up.png",
-    {
-      frameWidth: 16,
-      frameHeight: 16,
-    });
+    this.load.spritesheet(
+      "explosion",
+      "game-2/assets/spritesheets/explosion.png",
+      {
+        frameWidth: 16,
+        frameHeight: 16
+      }
+    );
+    this.load.spritesheet(
+      "power-up",
+      "game-2/assets/spritesheets/power-up.png",
+      {
+        frameWidth: 16,
+        frameHeight: 16
+      }
+    );
 
     // load player spritesheet
-    this.load.spritesheet("player", "game-2/assets/spritesheets/player.png",
-    {
+    this.load.spritesheet("player", "game-2/assets/spritesheets/player.png", {
       frameWidth: 16,
-      frameHeight: 24,
+      frameHeight: 24
     });
   }
 
@@ -48,5 +50,49 @@ class Scene1 extends Phaser.Scene {
     this.add.text(20, 20, "Loading game...");
     // jump to other scene
     this.scene.start("playGame");
+
+    this.loadAnimations();
+  }
+
+  loadAnimations() {
+    // animations for ships
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    // animations for power-ups
+    this.anims.create({
+      key: "red",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start: 0,
+        end: 1
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "gray",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start: 2,
+        end: 3
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
   }
 }
--- game-2/Scene2.js
+++ game-2/Scene2.js
@@ -4,7 +4,7 @@ class Scene2 extends Phaser.Scene {
     super("playGame");
   }
 
-  // method for loading ships sprites and animations
+  // method for loading ships sprites
   loadShips() {
     // adding sprites for ships
     this.ship1 = this.add.sprite(
@@ -22,49 +22,10 @@ class Scene2 extends Phaser.Scene {
     this.ship1.setOrigin(0, 0);
     this.ship2.setOrigin(0, 0);
     this.ship3.setOrigin(0, 0);
-
-    // create animations for ships
-    this.anims.create({
-      key: "ship1_anim",
-      frames: this.anims.generateFrameNumbers("ship"),
-      frameRate: 20,
-      repeat: -1
-    });
-    this.anims.create({
-      key: "ship2_anim",
-      frames: this.anims.generateFrameNumbers("ship2"),
-      frameRate: 20,
-      repeat: -1
-    });
-    this.anims.create({
-      key: "ship3_anim",
-      frames: this.anims.generateFrameNumbers("ship3"),
-      frameRate: 20,
-      repeat: -1
-    });
   }
 
-  // method for loading power-ups with animations etc.
+  // method for loading power-ups
   loadPowerUps() {
-    // animations for power-ups
-    this.anims.create({
-      key: "red",
-      frames: this.anims.generateFrameNumbers("power-up", {
-        start: 0,
-        end: 1
-      }),
-      frameRate: 20,
-      repeat: -1
-    });
-    this.anims.create({
-      key: "gray",
-      frames: this.anims.generateFrameNumbers("power-up", {
-        start: 2,
-        end: 3
-      }),
-      frameRate: 20,
-      repeat: -1
-    });
     // for adding physics laws
     this.powerUps = this.physics.add.group();
 
